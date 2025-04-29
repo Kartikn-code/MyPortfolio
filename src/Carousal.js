2600941d6e98b43c0e03b97e0ab8131b2d24f2b7
kartikn-code <ping2karti@gmail.com>
2025-04-29 14:06:14 +0530

    committed

--- src/Carousal.js
+++ src/Carousal.js
@@ -3,16 +3,16 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import pic1 from './images/full stack course.png';
-import pic2 from './images/GAO 1.png';
-import pic3 from './images/GAO 2.png';
-import pic4 from './images/GAO 3.png';
-import pic5 from './images/PRODIGY.png';
+import pic1 from "./images/full stack course.png";
+import pic2 from "./images/GAO 1.png";
+import pic3 from "./images/GAO 2.png";
+import pic4 from "./images/GAO 3.png";
+import pic5 from "./images/PRODIGY.png";
 
 const Carousel = () => {
   return (
     <div className="achievements container lg-12 md-12 sm-12 carousel-3d">
-        <h1>Achievements</h1>
+      <h1>Achievements</h1>
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={10}
